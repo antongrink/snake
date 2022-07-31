1075e741a24799e2f829522d2e42902ccd9e4c59
l <you@example.com>
2022-07-31 11:40:15 +0300

    commit snake

---
+++ snake.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <QWidget>
+#include <QKeyEvent>
+
+class Snake : public QWidget {
+
+  Q_OBJECT
+
+  public:
+      Snake(QWidget *parent = 0);
+
+  protected:
+      void paintEvent(QPaintEvent *);
+      void timerEvent(QTimerEvent *);
+      void keyPressEvent(QKeyEvent *);
+
+  private slots: void initGame();
+
+  private:
+      QImage dot;
+      QImage head;
+      QImage apple;
+
+      static const int B_WIDTH = 300;
+      static const int B_HEIGHT = 300;
+      static const int DOT_SIZE = 10;
+      static const int ALL_DOTS = 900;
+      static const int RAND_POS = 29;
+      static const int DELAY = 100;
+
+      int timerId;
+      int dots;
+      int apple_x;
+      int apple_y;
+
+      int x[ALL_DOTS];
+      int y[ALL_DOTS];
+
+      bool leftDirection;
+      bool rightDirection;
+      bool upDirection;
+      bool downDirection;
+      bool inGame;
+
+      void loadImages();
+
+
+      void locateApple();
+      void checkApple();
+      void checkCollision();
+      void move();
+      void doDrawing();
+      void gameOver(QPainter &);
+};
